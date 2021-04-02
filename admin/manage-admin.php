b39b37149ca433d27fd0b4f86b0ea8d0a8d702ff
Mosarrat Shazia Kabir <81244301+mosarrat@users.noreply.github.com>
2021-04-02 20:26:38 +0600

    database

---
+++ admin/manage-admin.php
@@ -0,0 +1,12 @@
+<?php include('partials/menu.php');?>
+
+       <!--Main Contain section starts-->
+       <div class="main-contain">
+            <div class ="wrapper">
+               <h1>Manage Admin</h1>
+               
+            </div>
+        </div>
+       <!--Main Contain section ends-->
+       
+<?php include('partials/footer.php');?>
