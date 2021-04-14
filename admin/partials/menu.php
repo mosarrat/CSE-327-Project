ba181a14833c004363a5fde6559826428bf99fd5
Mosarrat Shazia Kabir <81244301+mosarrat@users.noreply.github.com>
2021-04-15 04:28:21 +0600

    Add files via upload

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -14,7 +14,7 @@
                <li><a href="index.php">Home</a></li>
                <li><a href="manage-admin.php">Admin</a></li>
                <li><a href="manage-category.php">catagory</a></li>
-               <li><a href="manage-service.php">Services</a></li>
+               <li><a href="manage-service.php">Services</a></li> 
                <li><a href="manage-appoinment.php">Appoinment</a></li>
             </ul>
             </div>   
